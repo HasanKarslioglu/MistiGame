bee11565c90996e437aabd6b4746c146e8b56fa8
Eser Poyraz <eserela35@gmail.com>
2023-05-07 11:41:46 +0300

    updated

--- Misti/src/ComputerPlayer.java
+++ Misti/src/ComputerPlayer.java
@@ -1,23 +1,8 @@
+import java.util.ArrayList;
+
 public class ComputerPlayer extends Player {
 
-    //Buradaki choose değişkeni basic düzeyde direkt 0ı gösteriyor yani computer hiçbir
-    //Plan program yapmadan hep ilk kartı atıyor... Bu deneme süreci olduğu için böyle.
-
-    //Bu sınıftan extend edilecek olan novice bot sınıfı choose değişkenini rastgele belirleyecek ve sadece
-    //Bu değişkeni değiştirecek geri kalan algoritma aynı olacak
-
-    //Bu sınıftan extend edilecek olan regular bot sınıfı choose değişkenini daha isabetli belirleyecek.
-    //Yerde aynı sayı varsa atacak yoksa ve yerde joker varsa atacak o da yoksa rasgele kart atacak.
-
-    //Yine bu sınıftan üretilecek olan expert bot ise daha iyi oynayacak. (Bütün oyunda o zamana kadar atılmış olan
-    // kartları hafızasında tutacak örneğin 2.turdayız ve yere daha önce 3 defa 7 atıldı bu botumuzda da 7 var ise onu
-    // atacak çünkü kimse alamaz bu sayede)
-
-    //Diğer sınıflar sadece choose belirlemeye yarayacak kısacası... bu sayede alttaki playCard() fonksiyonu tamamen
-    //aynı olacak.
-
-    //UNUTMA IMPORTANT PLAYER CLASSINDAKİ BÜTÜN PRİVATE OLMAYAN DEĞİŞKENLER VE METHODLAR BU SINIFTA DA GEÇERLİ
-    protected int choose = 0;
+    protected int[] possibleCardsPoint=new int[4];
 
     //-----------CONSTRUCTORS------------
     public ComputerPlayer(String name){
@@ -25,5 +10,28 @@ public class ComputerPlayer extends Player {
     }
 
     //-----------METHODS------------
+
+    protected void simulatePlaycard(){
+        int[] cardPoints = new int[getHandCards().size()];
+
+
+        for (int i=0;i<getHandCards().size();i++ ){
+            boardCardRef.add(getHandCards().get(i));
+
+            if(isCollectable() == false){
+                cardPoints[i] = 0;
+
+            }else{
+                cardPoints[i]= ( super.calculateBoardPoints() );
+                possibleCardsPoint[i]= cardPoints[i];
+
+                System.out.println("Possible card: "+getHandCards().get(i).getCardFace()+
+                        " Player will get: "+cardPoints[i]+" point.");
+            }
+            boardCardRef.remove(boardCardRef.size()-1);
+
+        }
+
+    }
 
 }
--- Misti/src/RegularBot.java
+++ Misti/src/RegularBot.java
@@ -2,108 +2,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 
-public class RegularBot extends ComputerPlayer{
+public class RegularBot extends ComputerPlayer {
     public RegularBot(String name) {
         super(name);
     }
 
     @Override
     public void playCard() {
-        int indexOfJokerCard = isThereJoker();
-        simulatePlayCard();
-
-        if(indexOfJokerCard != -1 && boardCardRef.size() > 0) {
-            choose = indexOfJokerCard;
+        super.simulatePlaycard();
+        for (int i = 0; i < possibleCardsPoint.length; i++) {
+            if (possibleCardsPoint[i] > 0) {
+                choosedCard = possibleCardsPoint[i];
+            }
         }
+
 
         super.playCard();
     }
 
-    private ArrayList<Card> isThereMatchingCard(){
-        if (boardCardRef.size() == 0) return null;
-       ArrayList<Card> matchedCards=new ArrayList<>();
-        for (int i = 0; i < getHandCards().size(); i++) {
-            if (boardCardRef.get(boardCardRef.size() - 1).getCardFace().equals(getHandCards().get(i).getCardFace())){
-                matchedCards.add(getHandCards().get(i));
-
-        }
- }
-        if(matchedCards.size()==0){
-            return null;
-        }
-        return matchedCards;
-
-    }
-
-    private int isThereJoker(){
-        if (boardCardRef.size() == 0) return -1;
-        for (int i = 0; i < getHandCards().size(); i++) {
-            if (handCards.get(i).getCardFace().equals("J"))
-                return i;
-        }
-        //-1 means there is no matching with last board card
-        return -1;
-    }
-
-    private int simulatePlayCard(){
-       //matched cards will collect from isThereMatchingCard() method and will add in simulateCard's matchedCards arraylist.
-        ArrayList<Card> matchedCards= isThereMatchingCard();
-
-        //if there is no matched cards then it will check for joker
-       if(matchedCards!=null) {
-           //if there is matched card or cards then we will calculate each cards total points with board cards.
-           int[] cardPoints = new int[getHandCards().size()];
-
-
-           ArrayList<Card> maxPointCards = new ArrayList<Card>();
-           
-           for (int i = 0; i < cardPoints.length; i++) {
-               cardPoints[i] = super.calculateBoardPoints();
-               cardPoints[i] += getHandCards().get(i).getCardPoint();
-           }
-           //after calcultaion method will sort points small to large Value.
-           Arrays.sort(cardPoints);
-
-           int maxPointValue = cardPoints[0];
-           //Here the loop will control if the cardpoints are bigger than the smallest value and bigger than 0
-           for (int i = 1; i < cardPoints.length; i++) {
-               if (cardPoints[i] > 0 && cardPoints[i] >= maxPointValue) {
-                   maxPointValue = cardPoints[i];
-                   maxPointCards.add(getHandCards().get(i));
-               }
-           }
-           //It will check if there is maxpointcards and this card or cards will also in matchedCards.
-
-             for(int i=0;i<matchedCards.size();i++){
-                 if(maxPointCards.contains(matchedCards.get(i))){
-                     for (int k = 0; k < maxPointCards.size(); k++) {
-                         if (matchedCards.get(i)==maxPointCards.get(k)) {
-                             for (int j = 0; j < getHandCards().size(); j++) {
-                                 if (maxPointCards.get(k) == getHandCards().get(j)) {
-                                     choose = j;
-                                 }
-                             }
-
-                         }
-                     }
-                          //if matched card does not in maxPointCards, then program will select another card except marched card.
-
-                     for (int j = 0; j < getHandCards().size(); j++) {
-                         if (matchedCards.get(i) != getHandCards().get(j)) {
-                                  choose = j;
-                              }
-                          }
-
-                  }
-
-
-           }
-
-       }
-
-   return choose;
-    }
-
-
-
 }
+
+
+
+
